ccd8ad9d9a96ae6be7877b3409ec26a73b1e897e
Alberto-Ibarra <alberto.e.ibarra@gmail.com>
2025-03-20 20:23:50 -0500

    update db

--- services/database.js
+++ services/database.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.SQL_HOST,
-    user: process.env.SQL_USER,
-    password: process.env.SQL_PW,
-    database: process.env.SQL_DATABASE
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise()
 
 export default pool
